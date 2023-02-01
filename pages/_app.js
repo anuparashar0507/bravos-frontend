a78723b08ea3cbf03b1ff9fa5f15723f78789d82
Anupam Parashar <anuparashar0507@gmail.com>
2023-02-01 13:19:56 +0530

    appjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
-
+import "../styles/globals.css";
+import {
+  ChakraProvider,
+  ColorModeProvider,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import DefaultLayout from "@/components/Layout/DefaultLayout";
+import "@fontsource/montserrat";
+import { theme } from "@lib/theme";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <ColorModeProvider
+        options={{ initialColorMode: "dark", useSystemColorMode: true }}
+      />
+      <DefaultLayout>
+        <Component {...pageProps} />
+      </DefaultLayout>
+    </ChakraProvider>
+  );
 }
